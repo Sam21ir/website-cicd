72faba6196e3a7360d506dafe582e5828d70c876
Silas Teixeira <166008901+Silas-cloudspace@users.noreply.github.com>
2024-08-18 12:27:51 +0100

    Update cloudfront.tf

--- terraform/cloudfront.tf
+++ terraform/cloudfront.tf
@@ -1,9 +1,14 @@
-# Create the CloudFront distribution
+# cloudfront.tf
+
+resource "aws_cloudfront_origin_access_identity" "origin_access_identity" {
+  comment = "OAI for portfolio website"
+}
+
 resource "aws_cloudfront_distribution" "website_distribution" {
   origin {
     domain_name = aws_s3_bucket.website_bucket.bucket_regional_domain_name
-    origin_id   = "S3-silas-teixeira.com"
-    
+    origin_id   = "portfolio-bucket-st"
+
     s3_origin_config {
       origin_access_identity = aws_cloudfront_origin_access_identity.origin_access_identity.cloudfront_access_identity_path
     }
@@ -11,15 +16,15 @@ resource "aws_cloudfront_distribution" "website_distribution" {
 
   enabled             = true
   is_ipv6_enabled     = true
-  comment             = "CDN for silas-teixeira.com"
+  comment             = "Portfolio website"
   default_root_object = "index.html"
 
-  aliases = ["silas-teixeira.com", "www.silas-teixeira.com"]
+  aliases = ["www.silas-teixeira.com"]
 
   default_cache_behavior {
-    allowed_methods  = ["GET", "HEAD"]
+    allowed_methods  = ["GET", "HEAD", "OPTIONS"]
     cached_methods   = ["GET", "HEAD"]
-    target_origin_id = "S3-silas-teixeira.com"
+    target_origin_id = "portfolio-bucket-st"
 
     forwarded_values {
       query_string = false
@@ -29,15 +34,9 @@ resource "aws_cloudfront_distribution" "website_distribution" {
     }
 
     viewer_protocol_policy = "redirect-to-https"
-    min_ttl                = 0
-    default_ttl            = 3600
-    max_ttl                = 86400
-  }
-
-  viewer_certificate {
-    acm_certificate_arn      = aws_acm_certificate.cert.arn
-    ssl_support_method       = "sni-only"
-    minimum_protocol_version = "TLSv1.2_2021"
+    min_ttl                 = 0
+    default_ttl             = 3600
+    max_ttl                 = 86400
   }
 
   restrictions {
@@ -46,12 +45,9 @@ resource "aws_cloudfront_distribution" "website_distribution" {
     }
   }
 
-  tags = {
-    Name = "silas-teixeira.com"
+  viewer_certificate {
+    acm_certificate_arn            = aws_acm_certificate.website_certificate.arn
+    ssl_support_method             = "sni-only"
+    minimum_protocol_version       = "TLSv1.2_2021"
   }
-}
-
-# Create a CloudFront origin access identity
-resource "aws_cloudfront_origin_access_identity" "origin_access_identity" {
-  comment = "Access identity for S3 bucket silas-teixeira.com"
 }
