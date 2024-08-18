66d84809549c762e3feca7903d0a323df1cfb0a4
Silas Teixeira <166008901+Silas-cloudspace@users.noreply.github.com>
2024-08-18 12:27:20 +0100

    Update backend.tf

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -3,7 +3,7 @@ terraform {
   backend "s3" {
     bucket         = "terraform-state-bucket-st"
     key            = "website/terraform.tfstate"
-    region         = "your region"
+    region         = "eu-west-2"
     dynamodb_table = "state-lock-table-st"
   }
 }
