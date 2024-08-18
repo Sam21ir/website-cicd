8dcacb8a78a59cf8dce0b7cf16244fe2d7dd8139
Silas Teixeira <166008901+Silas-cloudspace@users.noreply.github.com>
2024-08-18 12:28:18 +0100

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,6 +1,12 @@
-# Configure the AWS Provider for S3 and CloudFront
+# provider.tf
+
+# Default AWS Provider
 provider "aws" {
-  region = "eu-west-2"  # For S3 and Route 53
+  region = "eu-west-2"
 }
 
-
+# Aliased AWS Provider for us-east-1
+provider "aws" {
+  alias  = "us_east_1"
+  region = "us-east-1"
+}
