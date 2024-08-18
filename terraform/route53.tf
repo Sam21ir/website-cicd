7865817c8e8937766af486d67f1f6afb67235f29
Silas Teixeira <166008901+Silas-cloudspace@users.noreply.github.com>
2024-08-18 12:28:44 +0100

    Update route53.tf

--- terraform/route53.tf
+++ terraform/route53.tf
@@ -1,13 +1,13 @@
-# Define the Route 53 DNS zone
-data "aws_route53_zone" "dns_zone" {
-  name         = "silas-teixeira.com."
+# route53.tf
+
+data "aws_route53_zone" "main" {
+  name         = "silas-teixeira.com"
   private_zone = false
 }
 
-# Create a Route 53 record to point to the CloudFront distribution
-resource "aws_route53_record" "a_record" {
-  zone_id = data.aws_route53_zone.dns_zone.id
-  name    = "silas-teixeira.com"
+resource "aws_route53_record" "www" {
+  zone_id = data.aws_route53_zone.main.zone_id
+  name    = "www.silas-teixeira.com"
   type    = "A"
 
   alias {
@@ -15,4 +15,20 @@ resource "aws_route53_record" "a_record" {
     zone_id                = aws_cloudfront_distribution.website_distribution.hosted_zone_id
     evaluate_target_health = false
   }
+}
+
+resource "aws_route53_record" "acm_cert_validation" {
+  for_each = {
+    for dvo in aws_acm_certificate.website_certificate.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      type   = dvo.resource_record_type
+      value  = dvo.resource_record_value
+    }
+  }
+
+  zone_id = data.aws_route53_zone.main.zone_id
+  name    = each.value.name
+  type    = each.value.type
+  records = [each.value.value]
+  ttl     = 60
 }
